023a0538b1fb22f1b1916eb84ab1cfb5879a9b96
Will <williamperlichek@gmail.con>
2025-06-23 15:39:48 -0700

    more work on unprot client

--- docs/uniprot.md
+++ docs/uniprot.md
@@ -4,6 +4,7 @@
 - https://www.uniprot.org/api-documentation/uniprotkb
 
 ## General Documentation
+
 - https://www.uniprot.org/uniprotkb/P35498/entry (Example of UnitProt info in their UI, contains OrthoDB groups too which is helpful for as a shortcut on orthodb.org which can be... flaky)
 
 ## Sample Request
--- src/comparative-genomic-analysis/client/uniprot_client.py
+++ src/comparative-genomic-analysis/client/uniprot_client.py
@@ -1,4 +1,5 @@
-import httpx
+import httpx, logging
+
 
 class UniProtClient:
     BASE_URL = "https://rest.uniprot.org/uniprotkb/"
@@ -6,11 +7,21 @@ class UniProtClient:
     def __init__(self):
         self.client = httpx.AsyncClient()
 
-    async def fetch_protein(self, query: str) -> str:
-        url = f"{self.BASE_URL}search?query={query}&format=fasta"
-        response = await self.client.get(url)
-        response.raise_for_status()
-        return response.text
+    async def fetch_protein_fasta_sequence_by_accession_id(
+        self, accession_id: str = ""
+    ) -> str:
+
+        if accession_id == "":
+            logging.warning(
+                "Must provide protein's accession id to get its fasta sequence"
+            )
+            return ""
+        else:
+            url = f"{self.BASE_URL}{accession_id}"
+            headers = {"Accept": "text/x-fasta"}
+            response = await self.client.get(url, headers=headers)
+            response.raise_for_status()
+            return response.text
 
     async def close(self):
         await self.client.aclose()
