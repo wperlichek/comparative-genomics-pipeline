9323d87a9964f8558ffe14e1a416ca2e47631941
Will <williamperlichek@gmail.com>
2025-06-24 09:51:49 -0700

    start to iterate on genes from file

--- src/comparative_genomics_pipeline/__main__.py
+++ src/comparative_genomics_pipeline/__main__.py
@@ -4,6 +4,7 @@ from .client import UniProtClient, NCBIClient
 
 logger = logging.getLogger(__name__)
 
+
 async def async_main():
     accession_id = "G1SSP8"
     entrez_protein_id = "XP_051685089.1"
@@ -11,13 +12,19 @@ async def async_main():
     ncbi_client = NCBIClient()
 
     print("Data input path: " + f"{path_config.DATA_INPUT_DIR}/genes_to_proteins.json")
-    
-    with open(f"{path_config.DATA_INPUT_DIR}/genes_to_proteins.json", "r") as f:
-        data = json.load(f)
-        print("Got data: " + str(data))
 
-    fasta_sequence_accession = await uni_prot_client.fetch_protein_fasta_sequence_by_accession_id(accession_id)
-    fasta_sequence_entrez = await ncbi_client.fetch_protein_fasta_by_entrez_id(entrez_protein_id)
+    with open(f"{path_config.DATA_INPUT_DIR}/genes_to_proteins.json", "r") as f:
+        genes = json.load(f)
+        for gene_name, ortholog_list in genes.items():
+            print(gene_name)
+            print(ortholog_list)
+
+    fasta_sequence_accession = (
+        await uni_prot_client.fetch_protein_fasta_sequence_by_accession_id(accession_id)
+    )
+    fasta_sequence_entrez = await ncbi_client.fetch_protein_fasta_by_entrez_id(
+        entrez_protein_id
+    )
     print(f"FASTA sequence for {accession_id}:\n{fasta_sequence_accession}")
     print(f"FASTA sequence for {entrez_protein_id}:\n{fasta_sequence_entrez}")
     pass
