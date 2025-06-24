a71ceacec892694bb9c9bef6bba42b359df44886
will Perlichek <willi@LAPTOP-Q5279NI9.localdomain>
2025-06-23 18:16:46 -0700

    more client

--- src/comparative_genomics_pipeline/__main__.py
+++ src/comparative_genomics_pipeline/__main__.py
@@ -1,9 +1,14 @@
 import asyncio, logging
 from .config import logging_config
+from .client import UniProtClient
 
 logger = logging.getLogger(__name__)
 
 async def async_main():
+    accession_id = "G1SSP8"
+    uni_prot_client = UniProtClient()
+    fasta_sequence = await uni_prot_client.fetch_protein_fasta_sequence_by_accession_id(accession_id)
+    print(f"FASTA sequence for {accession_id}:\n{fasta_sequence}")
     pass
 
 
