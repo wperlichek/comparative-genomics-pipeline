544044142c880695c3b0b96d180e9113745b8e92
wperlichek <williamperlichek@gmail.com>
2025-06-24 08:16:23 -0700

    getting fasta sequences from NCBI

--- src/comparative_genomics_pipeline/__main__.py
+++ src/comparative_genomics_pipeline/__main__.py
@@ -1,17 +1,18 @@
 import asyncio, logging
 from .config import logging_config
-from .client import UniProtClient
+from .client import UniProtClient, NCBIClient
 
 logger = logging.getLogger(__name__)
 
 async def async_main():
     accession_id = "G1SSP8"
+    entrez_protein_id = "XP_051685089.1"
     uni_prot_client = UniProtClient()
-    fasta_sequence = await uni_prot_client.fetch_protein_fasta_sequence_by_accession_id(accession_id)
-    print(f"FASTA sequence for {accession_id}:\n{fasta_sequence}")
-    # TODO ::    
-    #            Need client for entrez_protein_id (Biopython could work)
-    #            Need client for MSA (European bioinformatics Clustal Omega REST API)
+    ncbi_client = NCBIClient()
+    fasta_sequence_accession = await uni_prot_client.fetch_protein_fasta_sequence_by_accession_id(accession_id)
+    fasta_sequence_entrez = await ncbi_client.fetch_protein_fasta_by_entrez_id(entrez_protein_id)
+    print(f"FASTA sequence for {accession_id}:\n{fasta_sequence_accession}")
+    print(f"FASTA sequence for {entrez_protein_id}:\n{fasta_sequence_entrez}")
     pass
 
 
--- src/comparative_genomics_pipeline/client/__init__.py
+++ src/comparative_genomics_pipeline/client/__init__.py
@@ -1,5 +1,6 @@
 # __init__.py
 
 from .uniprot_client import UniProtClient
+from .ncbi_client import NCBIClient
 
-__all__ = ["UniProtClient"]
+__all__ = ["UniProtClient", "NCBIClient"]
