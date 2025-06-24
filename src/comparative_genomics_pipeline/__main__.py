2394f493385d28b18356faea39f1c4c06205e5e5
wperlichek <williamperlichek@gmail.com>
2025-06-24 08:50:22 -0700

    getting json protein info

--- src/comparative_genomics_pipeline/__main__.py
+++ src/comparative_genomics_pipeline/__main__.py
@@ -9,9 +9,12 @@ async def async_main():
     entrez_protein_id = "XP_051685089.1"
     uni_prot_client = UniProtClient()
     ncbi_client = NCBIClient()
+
+    print("Data input path: " + f"{path_config.DATA_INPUT_DIR}/genes_to_proteins.json")
     
-    with open("{path_config.DATA_INPUT_DIR}/genes_to_proteins.json", "r") as f:
+    with open(f"{path_config.DATA_INPUT_DIR}/genes_to_proteins.json", "r") as f:
         data = json.load(f)
+        print("Got data: " + str(data))
 
     fasta_sequence_accession = await uni_prot_client.fetch_protein_fasta_sequence_by_accession_id(accession_id)
     fasta_sequence_entrez = await ncbi_client.fetch_protein_fasta_by_entrez_id(entrez_protein_id)
